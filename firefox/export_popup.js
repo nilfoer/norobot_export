e710211ea925b4a3682b2bb201c1be4d7350544e
nilfoer <60221153+nilfoer@users.noreply.github.com>
2021-02-01 04:30:32 +0100

    try using contextualIdentities to display the buttons

--- firefox/export_popup.js
+++ firefox/export_popup.js
@@ -25,25 +25,41 @@ function handleMessage(req, sender, sendResponse) {
         }
 
         for (store of req.stores) {
-            // let li = document.createElement("div");
-            // li.classList.add("panel-list-item");
-            // let button = document.createElement("div");
-            // button.classList.add("button");
-            // button.classList.add("get-cookie-store");
-            // // add data-store-id attrib
-            // button.dataset.storeId = store.id;
-            // let textnode = document.createTextNode(store.id);
-            // button.appendChild(textnode);
-
-            // li.appendChild(button);
-            // container.appendChild(li);
-
             let button = document.createElement("div");
             button.classList.add("button");
             button.classList.add("get-cookie-store");
             // add data-store-id attrib
             button.dataset.storeId = store.id;
             let textnode = document.createTextNode(store.id);
+            button.appendChild(textnode);
+
+            container.appendChild(button);
+            
+            // click event
+            button.addEventListener("click", handleButtonClick);
+        }
+    } else if (req.action == "send_contexts") {
+        // apparently there are no contextualIdentities for default or private mode
+        // so can't really use that
+        // alternatively we could pass the contexts along with the store
+        // and if the storeIds match then use the context for name and color
+        console.log("Got send_stores", req.contexts);
+
+        let container = document.getElementById("container");
+        // remove all previous
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        }
+
+        for (ctx of req.contexts) {
+            let button = document.createElement("div");
+            button.classList.add("button");
+            button.classList.add("get-cookie-store");
+            // add data-store-id attrib
+            button.dataset.storeId = ctx.cookieStoreId;
+            button.style.backgroundColor = ctx.colorCode;
+
+            let textnode = document.createTextNode(ctx.name);
             button.appendChild(textnode);
 
             container.appendChild(button);
